a90c8fd66365a098db1b98c4e4138cecdf20d27d
242209 <242209@edu.p.lodz.pl>
2023-11-10 15:14:55 +0100

    reading and parsing experiment data

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
-# This is a sample Python script.
+from reader import loadData
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+data = loadData("Database_Scenario1 - Zigbee.csv", "Pathloss_Scenario1 - Zigbee.csv", "Tests_Scenario1 - Zigbee.csv")
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(data.database, data.tests,data.path_loss)
---
+++ reader.py
@@ -0,0 +1,73 @@
+import csv
+import dataclasses
+from dataclasses import dataclass
+from typing import List
+
+
+@dataclass
+class Entry:
+    def __post_init__(self):
+        for field in dataclasses.fields(self):
+            value = getattr(self, field.name)
+            if not isinstance(value, field.type):
+                setattr(self, field.name, field.type(value))
+
+
+@dataclass
+class TestEntry(Entry):
+    point: int
+    x: float
+    y: float
+    RSSI_A: int
+    RSSI_B: int
+    RSS_C: int
+
+
+@dataclass
+class DatabaseEntry(Entry):
+    x: float
+    y: float
+    RSSI_A: int
+    RSSI_B: int
+    RSS_C: int
+
+
+@dataclass
+class PathlossEntry(Entry):
+    distance: float
+    RSSI: int
+    point: int
+
+
+class ScenarioData:
+    def __init__(self, path_loss, tests, database):
+        self.path_loss: List[PathlossEntry] = path_loss
+        self.tests: List[TestEntry] = tests
+        self.database: List[DatabaseEntry] = database
+
+
+def loadData(db_filename: str, path_filename: str, tests_filename: str) -> ScenarioData:
+    database = []
+    with open(db_filename) as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        next(reader, None)
+        for row in reader:
+            database.append(DatabaseEntry(*row))
+
+    pathloss = []
+    with open(path_filename) as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        next(reader, None)
+        for row in reader:
+            pathloss.append(PathlossEntry(*row))
+
+    tests = []
+    with open(tests_filename) as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        next(reader, None)
+        for row in reader:
+            tests.append(TestEntry(*row))
+
+    return ScenarioData(pathloss, tests, database)
+
+

eb9a39a6e6b951510f2b4d759520028ee8645bd2
arturdenderski <artur.denderski@gmail.com>
2023-11-24 12:43:59 +0100

    show legend

--- visualize.py
+++ visualize.py
@@ -4,7 +4,7 @@ from reader2 import Runs, run_data
 
 def visualize_rssi_2d_matplotlib_all_runs(device: int):
     runs = Runs()
-    num_runs = 4  # number of runs
+    num_runs = 4
 
     num_rows = (num_runs + 1) // 2
     num_cols = 2
@@ -21,19 +21,24 @@ def visualize_rssi_2d_matplotlib_all_runs(device: int):
         for coord in runs.coords:
             x.append(coord[0])
             y.append(coord[1])
-            rssi.append(runs.get_rssi(coord, device, run))
+            rssi_val = runs.get_rssi(coord, device, run)
+            rssi.append(rssi_val)
 
         x, y, rssi = np.array(x), np.array(y), np.array(rssi)
 
-        ax.scatter(x, y, c=rssi, cmap='viridis', alpha=0.7)
+        sc = ax.scatter(x, y, c=rssi, cmap='viridis', alpha=0.7, label='RSSI Strength')
         ax.set_title(f"Run {run}")
         ax.set_xlabel('X-coordinate')
         ax.set_ylabel('Y-coordinate')
         ax.set_aspect('equal')
         ax.grid(True)
 
+        if run == 1:
+            ax.legend()
+
     plt.suptitle(f"RSSI Strength for Device {device}")
     plt.tight_layout(rect=[0, 0.03, 1, 0.95])
+    plt.colorbar(sc, ax=axes, orientation='vertical', label='RSSI Strength')
     plt.show()
 
 if __name__ == "__main__":
